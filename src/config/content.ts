a1b2e2ae61a9d3c8d8ef849aa7bad0b7b7574dcd
Mode-s <shota.umemoto7@gmail.com>
2026-06-24 16:42:04 +0900

    add 設備：シャンプー/トリートメント/ボディソープ、タオル、荷物配送サービス

--- src/config/content.ts
+++ src/config/content.ts
@@ -10,10 +10,11 @@ import item02 from '../assets/images/in-room08.png';
 import item03 from '../assets/images/in-room16.png';
 import item04 from '../assets/images/in-room17.png';
 import item05 from '../assets/images/in-room04.png';
+import item06 from '../assets/images/in-room18.png';
 
 type Logo = { src: ImageMetadata; alt: string; href: string };
 type NavItem = { label: string; href: string };
-type Item = { image: ImageMetadata; name: string; price?: string; description?: string };
+type Item = { images: ImageMetadata[]; name: string; price?: string; description?: string };
 type Room = { name: string; bedType: string; price: number; description: string };
 type Amenity = { label: string; icon: string };
 
@@ -36,11 +37,11 @@ export const content = {
   menu: {
     title: "お部屋",
     items: [
-      { image: item01, name: "シングルルーム", price: "￥12,000～", description: "ベッドタイプ：シングル" },
-      { image: item02, name: "ツインルーム", price: "￥14,500～", description: "ベッドタイプ：シングル２台" },
-      { image: item03, name: "ツインルーム", price: "￥14,500～", description: "ベッドタイプ：シングル２台" },
-      { image: item04, name: "ツインルーム", price: "￥14,500～", description: "ベッドタイプ：シングル２台" },
-      { image: item05, name: "トリプルルーム", price: "￥15,000～", description: "布団" },
+      { images: [item01, item06], name: "シングルルーム", price: "￥12,000～", description: "ベッドタイプ：シングル" },
+      { images: [item02], name: "ツインルーム", price: "￥14,500～", description: "ベッドタイプ：シングル２台" },
+      { images: [item03], name: "ツインルーム", price: "￥14,500～", description: "ベッドタイプ：シングル２台" },
+      { images: [item04], name: "ツインルーム", price: "￥14,500～", description: "ベッドタイプ：シングル２台" },
+      { images: [item05], name: "トリプルルーム", price: "￥15,000～", description: "布団" },
     ] satisfies Item[],
   },
   price: {
@@ -57,6 +58,9 @@ export const content = {
       { label: 'エアコン', icon: 'mdi:air-conditioner' },
       { label: '冷蔵庫', icon: 'mdi:fridge' },
       { label: 'ヘアドライヤー', icon: 'mdi:hair-dryer' },
+      { label: 'シャンプー / トリートメント / ボディソープ', icon: 'mdi:lotion'},
+      { label: 'タオル', icon: 'ph:towel-fill' },
+      { label: '荷物配送サービス', icon: 'mdi:truck-delivery'},
     ] satisfies Amenity[],
   },
 } as const;
